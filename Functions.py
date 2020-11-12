adb254061ef9f8e36647209288749e6711ab4bbb
Pythoncraft <73964453+Pythoncraft@users.noreply.github.com>
2020-11-12 17:08:55 +0200

    practice fuctions with parameters and arguments

---
+++ Functions.py
@@ -0,0 +1,16 @@
+def fag(): #defining function / can add parameter to the function 'First_name'
+	print('Hi there!')
+	print('Welcome aboard')
+
+print('Hello')
+fag() # when function is called NEED PARENTHESES ()
+print('Finish')
+
+
+def fag(First_name, Last_name): #defining function / can add parameter to the function 'First_name'
+	print(f'Hi {First_name} {Last_name}!')
+	print('Welcome aboard')
+
+
+fag("John", 'Smith') #when calling function need tot insert an argument in this case 'John'
+fag('Mary', 'Weatherspoon')
